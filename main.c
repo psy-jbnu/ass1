501e5e0dc4b4698722eb3193f4474044ef179b12
psy <keres1102@jbnu.ac.kr>
2025-09-28 23:42:37 +0900

    task 3 complete

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ int load_sensors_from_txt(char *path, struct sensor *sensors);
 int load_sensors_from_bin(char *path, struct sensor *sensors);
 void print_sensors(struct sensor *sensors);
 int save_sensors(char *path, struct sensor *sensors);
-
+int find_sensor(int id, struct sensor *sensors);
+int edit_sensor(int idx, struct sensor *sensors, int id, char * name, char *status);
 int main(int argc, char *argv[]) {
 	int result = 0;
 	struct sensor sensors[PCS];
@@ -52,6 +53,33 @@ int main(int argc, char *argv[]) {
                 }
 		print_sensors(sensors);
 
+	}
+	else if (strcmp(argv[1], "update") == 0){
+                if(argc != 6) {
+                        printf("잘못된 사용");
+                        result = -1;
+                        return result;
+                }
+		int idx = 0;
+		if(load_sensors_from_bin(argv[2], sensors) < PCS) {
+                        printf("파일 입력 에러");
+                        result = -1;
+                        return result;
+                }
+		idx = find_sensor(atoi(argv[3]), sensors);
+		if(idx < 0) {
+			printf("센서 없음.");
+			result = -1;
+			return result;
+		}
+		print_sensors(sensors);
+		edit_sensor(idx, sensors, atoi(argv[3]), argv[4], argv[5]);
+		print_sensors(sensors);
+		if(save_sensors(argv[2], sensors) < PCS) {
+                        printf("파일 출력 에러");
+                        result = -1;
+                        return result;
+                }
 	}
 	return result;
 	
@@ -99,4 +127,17 @@ int save_sensors(char *path, struct sensor *sensors) {
 	fclose(fptr);
 	return result;
 }
+int find_sensor(int id, struct sensor *sensors) {
+	int idx = -1;
+	for(int i = 0; i < PCS; i++) {
+                if(sensors[i].id == id) idx = i;	
+        }
+	return idx;
+}
 
+int edit_sensor(int idx, struct sensor *sensors, int id, char * name, char *status) {
+	if(sensors[idx].id != id) return -1;
+	if(strcmp(name, "-") != 0) strcpy(sensors[idx].name, name);
+	if(strcmp(status, "-") != 0) strcpy(sensors[idx].status, status);
+	return 0;
+}
