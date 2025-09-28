7b81d333aeb841c48e72a1718bd5381fdeb268fd
psy <keres1102@jbnu.ac.kr>
2025-09-28 22:32:02 +0900

    task1 구현 완료

--- main.c
+++ main.c
@@ -16,8 +16,30 @@ int save_sensors(char *path, struct sensor *sensors);
 int main(int argc, char *argv[]) {
 	int result = 0;
 	struct sensor sensors[PCS];
-	load_sensors_from_txt("/home/ubuntu/ass1/data.txt", sensors);
-	print_sensors(sensors);
+	if (strcmp(argv[1], "init") == 0){
+
+        	if(argc != 4) {
+            		printf("잘못된 사용");
+            		result = -1;
+            		return result;
+		}
+	
+		if(load_sensors_from_txt(argv[2], sensors) == -1) {
+			printf("파일 입력 에러");
+			result = -1;
+			return result;
+		}
+
+
+		if(save_sensors(argv[3], sensors) < PCS) {	
+			printf("파일 출력 에러");
+			result = -1;
+			return result;
+		}
+
+        }
+	return result;
+	
 }
 
 int load_sensors_from_txt(char path[], struct sensor sensors[]) {
@@ -43,3 +65,12 @@ void print_sensors(struct sensor *sensors) {
 	}
 }
 		
+int save_sensors(char *path, struct sensor *sensors) {
+	FILE *fptr = fopen(path, "wb");
+	int result = 0;
+	result = fwrite(sensors, sizeof(struct sensor), PCS, fptr);
+	fclose(fptr);
+	return result;
+}
+
+
